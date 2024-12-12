44a004ff0b4495cf5ae57f6e50f53881c4ddbe6c
Mohammad Nayan <105283967+MOHAMMAD-NAYAN@users.noreply.github.com>
2024-12-12 13:56:47 +0600

    Update auto.js

--- scripts/commands/auto.js
+++ scripts/commands/auto.js
@@ -17,7 +17,7 @@ handleEvent: async function ({ api, event, args }) {
     const fs = require("fs-extra")
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  const {alldown} = require("nayan-video-downloader")
+  const {alldown} = require("nayan-videos-downloader")
   if (body.startsWith("https://")) {
   api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
 const data = await alldown(content);
